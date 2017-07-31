a6ca661d95672377bcf1b8ff71a58e14f4c5cb44
hellisfull <30295806+hellisfull@users.noreply.github.com>
2017-07-31 16:07:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/myappcall/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hp.myappcall;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed;
+    Button bt;
+    private static final int REQUEST_INTERNET = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ed = (EditText) findViewById(R.id.editText);
+        bt = (Button) findViewById(R.id.bt);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number = ed.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setPackage("com.android.phone");
+                intent.setData(Uri.parse("tel:" + number));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+            }
+        });
+    }
+}
